2d4ce716b46886a2ae551632d3ddc1a29b40dce2
miaowuhtml <55978389+miaowuhtml@users.noreply.github.com>
2020-08-20 19:08:56 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,1 @@
-document.writeln("<iframe scrolling='no' frameborder='0' marginheight='0' marginwidth='0' width='100%' height='7350' allowTransparency src=http://ys.tuoguan.world/></iframe>");
+document.writeln("<iframe scrolling='no' frameborder='0' marginheight='0' marginwidth='0' width='100%' height='7350' allowTransparency src=http://ys.tuoguan.world/ys.html></iframe>");

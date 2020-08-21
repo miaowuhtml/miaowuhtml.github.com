fbf7a8af63209bbb6e656f14ee2ce4099ea5f710
miaowuhtml <55978389+miaowuhtml@users.noreply.github.com>
2020-08-21 23:01:36 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,1 @@
-document.writeln("<iframe scrolling='no' frameborder='0' marginheight='0' marginwidth='0' width='100%' height='7350' allowTransparency src=http://dy.bukafuli.com/></iframe>");
+document.writeln("<iframe scrolling='no' frameborder='0' marginheight='0' marginwidth='0' width='100%' height='7350' allowTransparency src=http://dy.bukafuli.com:88></iframe>");

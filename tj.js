a8e823bb29d80ef5d465dbdc923a6c215a5e4c5c
miaowuhtml <55978389+miaowuhtml@users.noreply.github.com>
2020-08-21 23:05:04 +0800

    Update tj.js

--- tj.js
+++ tj.js
@@ -1,1 +1,1 @@
-document.write ('<script type="text/javascript" src="https://v1.cnzz.com/z_stat.php?id=1279199173&web_id=1279199173"></script>');
+document.write ('<div style="display:none"><script type="text/javascript" src="https://v1.cnzz.com/z_stat.php?id=1279199173&web_id=1279199173"></script></div>');

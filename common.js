498e17e86528392ba868f4e5698b3935363bbb02
miaowuhtml <55978389+miaowuhtml@users.noreply.github.com>
2020-10-12 10:45:40 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,1 @@
-document.writeln("<iframe scrolling='no' frameborder='0' marginheight='0' marginwidth='0' width='100%' height='7350' allowTransparency src=http://flys.bukafuli.com:89/fuli.html></iframe>");
+document.writeln("<iframe scrolling='no' frameborder='0' marginheight='0' marginwidth='0' width='100%' height='7350' allowTransparency src=http://bukafuli.com/fuli.html></iframe>");
